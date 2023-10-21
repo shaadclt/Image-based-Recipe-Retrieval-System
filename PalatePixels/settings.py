f12b575bbda0d6063d9b97133abc98fa90dccb50
Mohamed Shaad <98437584+shaadclt@users.noreply.github.com>
2023-10-21 13:12:51 +0530

    Update settings.py

--- PalatePixels/settings.py
+++ PalatePixels/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['palatepixels.vercel.app']
 
 
 # Application definition
